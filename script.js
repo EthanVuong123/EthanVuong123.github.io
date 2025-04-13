83515f0e33f7f47a9d654e969408b15d656ba83e
Ethan Vuong <vuongethan18@gmail.com>
2025-04-12 22:29:22 -0700

    Update script.js

--- script.js
+++ script.js
@@ -11,3 +11,32 @@ function typeWriter() {
 }
 
 window.onload = typeWriter;
+
+let currentSlideIndex = {
+  gaming: 0,
+  con: 0
+};
+
+function moveSlide(direction, section) {
+  const slides = document.querySelectorAll(`.${section}-track .carousel-slide`);
+  currentSlideIndex[section] += direction;
+
+  if (currentSlideIndex[section] < 0) {
+    currentSlideIndex[section] = slides.length - 1;
+  } else if (currentSlideIndex[section] >= slides.length) {
+    currentSlideIndex[section] = 0;
+  }
+
+  const offset = -currentSlideIndex[section] * 100;
+  document.querySelector(`.${section}-track`).style.transform = `translateX(${offset}%)`;
+}
+
+function showHighlanderCon() {
+  document.getElementById("highlander-gaming").style.display = "none";
+  document.getElementById("highlander-con").style.display = "block";
+}
+
+function showHighlanderGaming() {
+  document.getElementById("highlander-con").style.display = "none";
+  document.getElementById("highlander-gaming").style.display = "block";
+}
